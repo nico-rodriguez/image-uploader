b0c2afdaf2c12c7dc9d5aeaf5b0d6e44b3beb650
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-09-04 16:47:44 -0300

    refactor: Remove duplicate require

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,6 @@ const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const errorHandler = require('./middleware/errorHandler');
-require('dotenv').config();
 
 // *********** Constants
 const app = express();
